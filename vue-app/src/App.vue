c968eeedce745968d7db04c628464252524b47ef
nuenook <nook.ittipon@gmail.com>
2020-01-11 16:11:42 +0700

    init vue app from cli

---
+++ vue-app/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <Header></Header>
+    <Ninjas v-bind:ninjas="ninjas" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Ninjas from "./components/Ninjas.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Footer,
+    Ninjas
+  },
+  data() {
+    return {
+      title: "Your first Vue file, woo",
+      ninjas: [
+        { name: "Ryu", speciality: "Vue Components", show: false },
+        { name: "Crystal", speciality: "HTML Wizardry", show: false },
+        { name: "Hitoshi", speciality: "Click Events", show: false },
+        { name: "Tango", speciality: "Conditionals", show: false },
+        { name: "Kami", speciality: "Webpack", show: false },
+        { name: "Yoshi", speciality: "Data Diggin", show: false }
+      ]
+    };
+  },
+  methods: {
+    greeting: function() {
+      return "Heeey cowboy!";
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
